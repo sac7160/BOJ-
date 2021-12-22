943a1cb50b39f29a77378ed3a93e15b6594a4fbc
Chang Hyeon Park <sac7160@naver.com>
2021-12-23 01:28:32 +0900

    2021.12.23_11279번 문제풀이

---
+++ 11279번/11279.cpp
@@ -0,0 +1,34 @@
+// 11279번
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(void)
+{
+    cin.sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    priority_queue<int> pQ;
+    int num;
+ 
+    
+    cin >> num;
+    
+    for(int i=0; i<num; i++)
+    {
+        int enterNum;
+        cin >> enterNum;
+        if(enterNum) pQ.push(enterNum);
+        else if(pQ.empty()) cout << '0' << '\n';
+        else
+        {
+            cout << pQ.top() << '\n';
+            pQ.pop();
+        }
+    }
+    return 0;
+}
+
