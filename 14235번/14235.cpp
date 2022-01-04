80fcccd2d5c1fe3fa7492b2aa84cd93187d6bd93
ChangHyeon <sac7160@naver.com>
2022-01-04 11:16:39 +0900

    2022.01.04_14235번 문제풀이

---
+++ 14235번/14235.cpp
@@ -0,0 +1,56 @@
+﻿//14235번
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+priority_queue<int> present;
+void print( )
+{
+	if (present.empty()) cout << -1 << '\n';
+	else
+	{
+		cout << present.top() << '\n';
+		present.pop();
+	}
+}
+
+int main()
+{
+	int n;
+	vector<int> nums;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int enter;
+		cin >> enter;
+		nums.push_back(enter);
+		if (enter == 0) continue;
+		else
+		{
+			for (int x = 0; x < enter; x++)
+			{
+				int j;
+				cin >> j;
+				nums.push_back(j);
+			}
+		}
+	}
+
+	for (int i = 0; i < nums.size(); i++)
+	{
+		if (nums[i] == 0) print();
+		else
+		{
+			int j = nums[i];
+			for (int x =1; x <= j; x++)
+			{
+				present.push(nums[i + x ]);
+			}
+			i = i + j;
+		}
+	}
+}
