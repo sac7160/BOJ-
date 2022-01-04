0c733bcec29c9545f861264a5295ef1140b4f82e
ChangHyeon <sac7160@naver.com>
2022-01-04 12:38:18 +0900

    2022.01.04_11920번 문제풀이

---
+++ 11920번/11920.cpp
@@ -0,0 +1,48 @@
+﻿//11920번
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	
+	priority_queue<int,vector<int>,greater<>> maxNums;
+	vector<int> ans;
+
+	while (n--)
+	{
+		int enter;
+		cin >> enter;
+		
+		if (maxNums.size() < k) maxNums.push(enter);
+		else
+		{
+			if (maxNums.top() <= enter)
+			{
+				ans.push_back(maxNums.top());
+				maxNums.pop();
+				maxNums.push(enter);
+			}
+			else
+			{
+				ans.push_back(enter);
+			}
+		}
+	}
+	for (int i = 0; i < ans.size(); i++)
+	{
+		cout << ans[i] << " ";
+	}
+	
+	while(!maxNums.empty())
+	{
+		cout << maxNums.top() << " ";
+		maxNums.pop();
+	}
+}
