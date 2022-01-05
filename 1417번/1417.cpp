0db8deafd113b0ec3013f2dbdf08509d4b0e67c5
ChangHyeon <sac7160@naver.com>
2022-01-05 10:31:26 +0900

    2022.01.05_1417번 문제풀이

---
+++ 1417번/1417.cpp
@@ -0,0 +1,36 @@
+﻿//1417번
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+
+int main()
+{
+	int n;
+	int x;
+	priority_queue<int> candidates;
+
+	cin >> n;
+	cin >> x;
+
+	for (int i = 1; i < n; i++)
+	{
+		int enter;
+		cin >> enter;
+		candidates.push(enter);
+	}
+	int count = 0;
+
+	while (!candidates.empty() && x <= candidates.top())
+	{
+		int tmp = candidates.top()-1;
+		candidates.pop();
+		candidates.push(tmp);
+		x++;
+		count++;
+	}
+	cout << count;
+
+}
