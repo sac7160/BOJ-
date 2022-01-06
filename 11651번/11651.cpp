ebdbb1f79871bcaabf82dc410e711dd93d8fc336
ChangHyeon <sac7160@naver.com>
2022-01-06 13:16:03 +0900

    2022.01.06_11651번 문제풀이

---
+++ 11651번/11651.cpp
@@ -0,0 +1,50 @@
+﻿//11651번
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+class Pair
+{
+public:
+	int x;
+	int y;
+};
+
+bool compare(Pair a, Pair b)
+{
+	if (a.y == b.y)
+		return a.x < b.x;
+	else
+		return a.y < b.y;
+}
+
+
+int main()
+{
+	vector<Pair> pairs;
+	int n;
+	cin >> n;
+
+
+	for (int i = 0; i < n; i++)
+	{
+		Pair newPair;
+		int x, y;
+		cin >> x >> y;
+		newPair.x = x;
+		newPair.y = y;
+		pairs.push_back(newPair);
+
+	}
+
+	sort(pairs.begin(), pairs.end(), compare);
+
+
+
+	for (int i = 0; i < pairs.size(); i++)
+	{
+		cout << pairs[i].x << " " << pairs[i].y << '\n';
+	}
+}
