065398d16e4c0332707648c237e7dd7404ec44c6
ChangHyeon <sac7160@naver.com>
2022-01-06 13:06:01 +0900

    2022.01.06_1181번 문제풀이

---
+++ 1181번/1181.cpp
@@ -0,0 +1,48 @@
+﻿//1181번
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+bool compare(string a, string b)
+{
+	if (a.size() != b.size())
+		return a.size() < b.size();
+	else
+		return a < b;
+}
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector <string> tmp;
+	for (int i = 0; i < n; i++)
+	{
+		string enter;
+		cin >> enter;
+		tmp.push_back(enter);
+	}
+
+	sort(tmp.begin(), tmp.end(), compare);
+	vector <string> ans;
+	ans.push_back(tmp[0]);
+
+	for (int i = 0; i < tmp.size()-1; i++)
+	{
+		if (tmp[i] == tmp[i + 1]) continue; 
+		else
+		{
+			ans.push_back(tmp[i + 1]);
+		}
+	}
+
+	for (int i = 0; i < ans.size(); i++)
+	{
+		cout << ans[i] << '\n';
+	}
+}
