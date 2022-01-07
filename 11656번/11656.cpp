e6ace4b8180c611655387346d896737dd66510c6
ChangHyeon <sac7160@naver.com>
2022-01-07 11:06:01 +0900

    2022.01.07_11656번 문제풀이

---
+++ 11656번/11656.cpp
@@ -0,0 +1,26 @@
+﻿//10814번
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+int main()
+{
+	using namespace std;
+
+	string s;
+	cin >> s;
+	int length = s.length();
+	vector<string> tmp;
+
+	for (int i = 0; i < length; i++)
+	{
+		tmp.push_back( s.substr(i, length - i));
+	}
+	sort(tmp.begin(), tmp.end());
+	for (int i = 0; i < tmp.size(); i++)
+	{
+		cout << tmp[i] << '\n';
+	}
+}
