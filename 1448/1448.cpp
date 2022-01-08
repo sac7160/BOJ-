0869c85599b0c5c13497db7c3a3343ad0dd14ea6
ChangHyeon <sac7160@naver.com>
2022-01-08 10:31:35 +0900

    2022.01.08_1448번 문제풀이

---
+++ 1448/1448.cpp
@@ -0,0 +1,36 @@
+﻿//1448번
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	
+	using namespace std;
+	int n;
+	int enter;
+	vector<int>nums;
+	cin >> n;
+	while (n--)
+	{
+		cin >> enter;
+		nums.push_back(enter);
+	}
+	sort(nums.begin(), nums.end(), greater<>());
+	
+	int maxIndex = 0;
+	for (int i = maxIndex+1; i < nums.size(); i++)
+	{
+		for (int j = i+1 ; j < nums.size(); j++)
+		{
+			if (nums[maxIndex] < nums[i] + nums[j])
+			{
+				cout << nums[maxIndex]+nums[i]+nums[j];
+				return 0;
+			}
+		}
+		maxIndex++;
+	}
+	cout << -1;
+}
