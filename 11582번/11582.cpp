1d2529995206c6cdb1dd0e07b9e42e0054fe4c6d
ChangHyeon <sac7160@naver.com>
2022-01-09 17:55:19 +0900

    2022.01.09_11582번 문제풀이

---
+++ 11582번/11582.cpp
@@ -0,0 +1,39 @@
+﻿//11582번
+
+#include <iostream>
+#include <vector>
+#include<algorithm>
+
+int main()
+{
+
+	using namespace std;
+	int n,enter,k;
+	cin >> n;
+	vector<int>nums;
+	vector<int> ans;
+
+	for(int i=0; i<n; i++)
+	{
+		cin >> enter;
+		nums.push_back(enter);
+	}
+
+	cin >> k;
+	int index = 0;
+
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = 0; j < n / k; j++)
+		{
+			ans.push_back(nums[index]);
+			index++;
+		}
+		sort(ans.begin(), ans.end());
+		for (int i = 0; i < ans.size(); i++)
+		{
+			cout << ans[i] << " ";
+		}
+		ans.clear();
+	}
+}
