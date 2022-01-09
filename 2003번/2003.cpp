d507408959efef17fb80e755ffef0aecf2b457e5
ChangHyeon <sac7160@naver.com>
2022-01-09 17:32:44 +0900

    2022.01.09_2003번 문제풀이

---
+++ 2003번/2003.cpp
@@ -0,0 +1,50 @@
+﻿//2003번
+
+#include <iostream>
+#include <vector>
+
+
+int main()
+{
+
+	using namespace std;
+	int n, m,enter;
+
+	cin >> n >> m;
+	vector<int>nums;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> enter;
+		nums.push_back(enter);
+	}
+
+	int sum = 0;
+	int startIndex = 0;
+	int ans = 0;
+	int index = 0;
+
+	while (startIndex != n)
+	{
+		if (sum == m)
+		{
+			ans++;
+			sum = 0;
+			startIndex++;
+			index = startIndex;
+		}
+		else
+		{
+			if (index == n) break;
+			sum += nums[index++];
+			if (sum > m)
+			{
+				startIndex++;
+				index = startIndex;
+				sum = 0;
+			}
+		}
+	}
+
+	cout << ans;
+}
