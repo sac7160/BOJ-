09dab1b9c3de672572e283dfff92cc7829e329ac
ChangHyeon <sac7160@naver.com>
2022-01-10 17:01:42 +0900

    2022.01.10_2822번 문제풀이

---
+++ 2822번/2822.cpp
@@ -0,0 +1,49 @@
+﻿//2822번
+
+#include <iostream>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+class ScoreIndex
+{
+public:
+	int score;
+	int index;
+};
+bool compare(ScoreIndex a, ScoreIndex b)
+{
+	return a.score > b.score;
+}
+
+int main()
+{
+	vector<ScoreIndex>score;
+	int enter;
+
+	for (int i = 0; i < 8; i++)
+	{
+		cin >> enter;
+		ScoreIndex x;
+		x.score = enter;
+		x.index = i;
+		score.push_back(x);
+	}
+	sort(score.begin(), score.end(), compare);
+	int sum = 0;
+	vector<int>index;
+
+	for (int i = 0; i < 5; i++)
+	{
+		sum += score[i].score;
+		index.push_back(score[i].index);
+	}
+	sort(index.begin(), index.end());
+
+	cout << sum << '\n';
+	for (int i = 0; i < 5; i++)
+	{
+		cout << index[i]+1 << " ";
+	}
+}
