1484e74ca18750f30a91cc9cc80f77fb0dcb4731
ChangHyeon <sac7160@naver.com>
2022-01-10 17:35:37 +0900

    2022.01.10_10867번 문제풀이

---
+++ 10867번/10867.cpp
@@ -0,0 +1,38 @@
+﻿//10867번
+
+#include <iostream>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int n,enter;
+	cin >> n;
+	vector<int> nums;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> enter;
+		nums.push_back(enter);
+	}
+
+	sort(nums.begin(), nums.end());
+	vector<int>ans;
+	ans.push_back(nums[0]);
+
+	for (int i = 1; i < n; i++)
+	{
+		if (ans.back() != nums[i]) ans.push_back(nums[i]);
+	}
+
+	for (int i = 0; i < ans.size(); i++)
+	{
+		cout << ans[i] << " ";
+	}
+	
+}
