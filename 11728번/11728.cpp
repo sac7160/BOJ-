8dc588468d4d64776228b53cfca630180ab930c4
ChangHyeon <sac7160@naver.com>
2022-01-10 17:19:58 +0900

    2022.01.10_11728번 문제풀이

---
+++ 11728번/11728.cpp
@@ -0,0 +1,30 @@
+﻿//11728번
+
+#include <iostream>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int n, m, enter;
+	cin >> n >> m;
+	vector<int> nums;
+
+	for (int i = 0; i < n + m; i++)
+	{
+		cin >> enter;
+		nums.push_back(enter);
+	}
+
+	sort(nums.begin(), nums.end());
+
+	for (int i = 0; i < n + m; i++)
+	{
+		cout << nums[i] << " ";
+	}
+}
