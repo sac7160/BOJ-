14b2943a9e23fb28507fbe482172926363aeb459
ChangHyeon <sac7160@naver.com>
2022-01-12 15:28:38 +0900

    2022.01.12_11931번 문제풀이

---
+++ 11931번/11931.cpp
@@ -0,0 +1,27 @@
+﻿//11931번
+
+#include <iostream>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+
+int main()
+{
+	int enter,n;
+	vector<int>nums;
+	
+	cin >> n;
+	while (n--)
+	{
+		cin >> enter;
+		nums.push_back(enter);
+	}
+
+	sort(nums.begin(), nums.end(), greater<>());
+	for (int i = 0; i < nums.size(); i++)
+	{
+		cout << nums[i] << '\n';
+	}
+}
