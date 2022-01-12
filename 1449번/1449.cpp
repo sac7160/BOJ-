4d5c01a7f25c680c038b5c43e0594a89c9248606
ChangHyeon <sac7160@naver.com>
2022-01-12 16:12:56 +0900

    2022.01.12_1449번 문제풀이

---
+++ 1449번/1449.cpp
@@ -0,0 +1,36 @@
+﻿//1449번
+
+#include <iostream>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n,l,enter;
+	cin >> n >> l;
+	vector<int>nums;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> enter;
+		nums.push_back(enter);
+	}
+
+	sort(nums.begin(), nums.end());
+
+	int count = 1;
+	int start = nums[0];
+
+	for (int i = 1; i < nums.size(); i++)
+	{
+		if (nums[i] - start < l) continue;
+		else
+		{
+			count++;
+			start = nums[i];
+		}
+	}
+	cout << count;
+}
