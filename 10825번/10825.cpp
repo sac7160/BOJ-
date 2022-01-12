a49f5f9e9a7df07e1524e9f229b8ae03945a5ddb
ChangHyeon <sac7160@naver.com>
2022-01-12 15:51:58 +0900

    2022.01.12_10825번 문제풀이

---
+++ 10825번/10825.cpp
@@ -0,0 +1,52 @@
+﻿//10825번
+
+#include <iostream>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+class student
+{
+public:
+	string name;
+	int korean;
+	int english;
+	int math;
+};
+bool compare(student a, student b)
+{
+	if (a.korean != b.korean) return a.korean > b.korean;
+	else if (a.english != b.english) return a.english < b.english;
+	else if (a.math != b.math) return a.math > b.math;
+	else return a.name < b.name;
+};
+
+int main()
+{
+	int enter,n;
+	string enterName;
+	vector<student>students;
+	
+	cin >> n;
+	while (n--)
+	{
+		student x;
+		cin >> enterName;
+		x.name = enterName;
+		cin >> enter;
+		x.korean = enter;
+		cin >> enter;
+		x.english = enter;
+		cin >> enter;
+		x.math = enter;
+		students.push_back(x);
+	}
+
+	sort(students.begin(), students.end(), compare);
+	
+	for (int i = 0; i < students.size(); i++)
+	{
+		cout << students[i].name << '\n';
+	}
+}
