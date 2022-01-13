3e136767fa77dbe8b94ba3f0f5ed9d950e49e39a
ChangHyeon <sac7160@naver.com>
2022-01-13 09:54:44 +0900

    2022.01.13_16212번 문제풀이

---
+++ 16212번/16212.cpp
@@ -0,0 +1,28 @@
+﻿//16212번
+
+#include <iostream>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n,enter;
+	cin >> n;
+	vector<int>nums;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> enter;
+		nums.push_back(enter);
+	}
+
+	sort(nums.begin(), nums.end());
+
+	for(int i=0; i<n; i++)
+	{
+		cout << nums[i] << " ";
+	}
+
+}
