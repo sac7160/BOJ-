023e84d9f409339c72935e779c3f36e3a22251ac
ChangHyeon <sac7160@naver.com>
2022-01-13 10:27:19 +0900

    2022.01.13_13164번 문제풀이

---
+++ 13164번/13164.cpp
@@ -0,0 +1,42 @@
+﻿//13164번
+
+#include <iostream>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n,k,enter;
+	cin >> n >> k;
+	vector<int>nums;
+	vector<int>distance;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> enter;
+		nums.push_back(enter);
+	}
+
+	for (int i = 0; i < n-1; i++)
+	{
+		distance.push_back(nums[i+1]-nums[i]);
+	}
+
+	sort(distance.begin(), distance.end(), greater<>());
+	int sum = 0;
+	for (int i = 0; i < n-1; i++)
+	{
+		sum += distance[i];
+	}
+	int ans = 0;
+
+	for(int i=0; i<k-1; i++)
+	{
+		ans += distance[i];
+	}
+
+	cout << sum-ans;
+
+}
