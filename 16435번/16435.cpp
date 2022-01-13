37dc738342e019e8c59cda2ada5faf7141d5b038
ChangHyeon <sac7160@naver.com>
2022-01-13 09:42:45 +0900

    2022.01.13_16435번 문제풀이

---
+++ 16435번/16435.cpp
@@ -0,0 +1,31 @@
+﻿//1449번
+
+#include <iostream>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n,l,enter;
+	cin >> n >> l;
+	vector<int>nums;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> enter;
+		nums.push_back(enter);
+	}
+
+	sort(nums.begin(), nums.end());
+
+	for (int i = 0; i < n; i++)
+	{
+		if (l >= nums[i]) l++;
+		else break;
+	}
+
+	cout << l;
+
+}
