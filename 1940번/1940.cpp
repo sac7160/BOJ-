93a9af8ead9ab34e65046cee8d4faf8a0bc0688b
ChangHyeon <sac7160@naver.com>
2022-01-14 11:03:28 +0900

    2022.01.14_1940번 문제풀이

---
+++ 1940번/1940.cpp
@@ -0,0 +1,39 @@
+﻿//1940번
+
+#include <iostream>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n,m,enter;
+	cin >> n >> m;
+	vector<int>nums;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> enter;
+		nums.push_back(enter);
+	}
+	sort(nums.begin(), nums.end());
+
+	int count = 0;
+
+	for (int i = 0; i < n-1; i++)
+	{
+		int j = 1;
+		while (i + j < n)
+		{
+			if (nums[i] + nums[i + j] == m)
+			{
+				count++;
+				break;
+			}
+			else j++;
+		}
+	}
+	cout << count;
+}
+
