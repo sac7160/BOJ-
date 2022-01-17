5efa6251df844e2e9a634911e9403e5c1c63c8d0
ChangHyeon <sac7160@naver.com>
2022-01-17 13:04:29 +0900

    2022.01.17_16916번 문제풀이

---
+++ 16916번/16916.cpp
@@ -0,0 +1,23 @@
+﻿//16916번
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char* p = new char[1000001];
+    char* s = new char[1000001];
+   
+
+    cin >> s >> p;
+
+    char* ptr = strstr(s,p);
+    if (ptr == NULL) cout << 0;
+    else cout << 1;
+
+    delete[] s;
+    delete[] p;
+
+}
