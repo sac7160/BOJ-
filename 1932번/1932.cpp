f0252d60466eb36bc082851adb5fa720d6ff317b
ChangHyeon <sac7160@naver.com>
2022-01-21 13:46:40 +0900

    2022.01.21_1932번 문제풀이

---
+++ 1932번/1932.cpp
@@ -0,0 +1,40 @@
+﻿//1932번
+
+#include <iostream>
+
+using namespace std;
+
+int index = 0;
+int triangle[501][501];
+int dp[501][501];
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n ; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cin >> triangle[i][j];
+		}
+	}
+
+	dp[1][1] = triangle[1][1];
+
+	for (int i = 2; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			if (j == 1) dp[i][j] = triangle[i][j] + dp[i - 1][j];
+			else if (j == i) dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+			else dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+		}
+	}
+	int max = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (dp[n][i] > max) max = dp[n][i];
+	}
+	cout << max;
+}
