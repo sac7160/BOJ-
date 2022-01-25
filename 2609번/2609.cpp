48e440b16ec6bc4354ef8a14d397e69a5c0fe890
ChangHyeon <sac7160@naver.com>
2022-01-25 23:28:33 +0900

    2022.01.25_2609번 문제풀이

---
+++ 2609번/2609.cpp
@@ -0,0 +1,35 @@
+﻿//2609번
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	int enter_x, enter_y;
+	int x,y;
+	int ans;
+
+	cin >> enter_x >> enter_y;
+	
+	x = enter_x;
+	y = enter_y;
+
+	int r = x % y;
+
+	while (true)
+	{
+		if (r == 0)
+		{
+			ans = y;
+			break;
+		}
+		else
+		{
+			x = y;
+			y = r;
+			r = x % y;
+		}
+	}
+	cout << ans << '\n' << enter_x*enter_y/ans;
+
+}
