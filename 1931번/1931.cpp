391d625645d37dbbe43f81752be5a07e9f53e1d2
ChangHyeon <sac7160@naver.com>
2022-01-26 11:40:01 +0900

    2022.01.26_1931번 문제풀이

---
+++ 1931번/1931.cpp
@@ -0,0 +1,53 @@
+﻿//1931번
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Class
+{
+public:
+	int startTime;
+	int endTime;
+};
+
+bool compare(Class a, Class b)
+{
+	if (a.endTime == b.endTime)
+	{
+		return a.startTime < b.startTime;
+	}
+	return a.endTime < b.endTime;
+}
+
+int main()
+{
+	using namespace std;
+
+	int n;
+	vector<Class> classes;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		Class tmp;
+		cin >> tmp.startTime >> tmp.endTime;
+		classes.push_back(tmp);
+	}
+
+	sort(classes.begin(), classes.end(), compare);
+
+	int count = 1;
+	int index = 0;
+
+	for (int i = 1; i < classes.size(); i++)
+	{
+		if (classes[i].startTime >= classes[index].endTime)
+		{
+			count++;
+			index = i;
+		}
+	}
+	cout << count;
+}
+
