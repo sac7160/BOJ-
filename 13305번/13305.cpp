beff9607a5afadd4cb98fa8ce5f296cff98a2fbf
ChangHyeon <sac7160@naver.com>
2022-01-27 10:36:22 +0900

    2022.01.27_13305번 문제풀이

---
+++ 13305번/13305.cpp
@@ -0,0 +1,39 @@
+﻿//13305번
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	using namespace std;
+
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int n,enter;
+	vector<int>length;
+	vector<int>price;
+
+	cin >> n;
+
+	for (int i = 1; i < n; i++)
+	{
+		cin >> enter;
+		length.push_back(enter);
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> enter;
+		price.push_back(enter);
+	}
+
+	long long min = price[0];
+	long long sum = 0;
+
+	for (int i = 1; i < n; i++)
+	{
+		if (price[i - 1] < min) min = price[i - 1];
+		sum += min*length[i-1];
+	}
+	cout << sum;
+}
