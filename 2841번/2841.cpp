d49a7b0c38565b35d9593eb30d62832388963cce
ChangHyeon <sac7160@naver.com>
2022-01-28 12:07:19 +0900

    2022.01.28_2841번 문제풀이

---
+++ 2841번/2841.cpp
@@ -0,0 +1,44 @@
+﻿//2841번
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	
+	int n, p;
+	cin >> n >> p;
+
+	stack<int> line[6];
+
+	int enter1, enter2;
+
+	int moves = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> enter1 >> enter2;
+
+		while (!line[enter1 - 1].empty())
+		{
+			if (line[enter1 - 1].top() > enter2)
+			{
+				line[enter1 - 1].pop();
+				moves++;
+			}
+			else break;
+		}
+		if (line[enter1 - 1].empty())
+		{
+			line[enter1 - 1].push(enter2);
+			moves++;
+			continue;
+		}
+		if (line[enter1 - 1].top() == enter2) continue;
+		line[enter1 - 1].push(enter2);
+		moves++;
+	}
+	cout << moves;
+}
