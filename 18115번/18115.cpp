66876b62a36d685aa639251ef0120a386400323f
ChangHyeon <sac7160@naver.com>
2022-01-29 14:41:16 +0900

    2022.01.29_18115번 문제풀이

---
+++ 18115번/18115.cpp
@@ -0,0 +1,48 @@
+﻿//18115번
+
+#include <iostream>
+#include <deque>
+#include <vector>
+
+int main()
+{
+	using namespace std;
+
+	int n,enter;
+	vector<int>skill;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> enter;
+		skill.push_back(enter);
+	}
+
+	int input = 1;
+	deque<int>card;
+
+	for (int i = 1;i <= n; i++)
+	{
+		if (skill[n-i] == 1)
+		{
+			card.push_front(input++);
+		}
+		else if (skill[n-i] == 2)
+		{
+			int tmp = card.front();
+			card.pop_front();
+			card.push_front(input++);
+			card.push_front(tmp);
+		}
+		else if (skill[n-i] == 3)
+		{
+			card.push_back(input++);
+		}
+	}
+
+	for (int i = 0; i < card.size(); i++)
+	{
+		cout << card[i] << " ";
+	}
+}
