dfe10dea0bfc1cff793bd8cc40d0275844b094bf
ChangHyeon <sac7160@naver.com>
2022-02-25 19:15:32 +0900

    2022.02.25_10845번 문제풀이

---
+++ 10845번/10845.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int n,enter2;
+	cin >> n;
+
+	string enter;
+	queue<int>nums;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> enter;
+		if (enter == "push") {
+			cin >> enter2;
+			nums.push(enter2);
+		}
+		else if (enter == "pop")
+		{
+			if (nums.size() == 0) cout << -1 << '\n';
+			else
+			{
+				cout << nums.front() << '\n';
+				nums.pop();
+			}
+		}
+		else if (enter == "size") cout << nums.size() << '\n';
+		else if (enter == "empty")
+		{
+			if (nums.size() == 0) cout << 1 << endl;
+			else cout << 0 << endl;
+		}
+		else if (enter == "front")
+		{
+			if (nums.size() == 0) cout << -1 << '\n';
+			else
+			{
+				cout << nums.front() << '\n';
+			}
+		}
+		else if (enter == "back")
+		{
+			if (nums.size() == 0) cout << -1 << '\n';
+			else
+			{
+				cout << nums.back() << '\n';
+			}
+		}
+
+	}
+}
