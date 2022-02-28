cea78cbfc2eaeacffb4f18a8e90b084fae709f73
ChangHyeon <sac7160@naver.com>
2022-02-28 11:03:48 +0900

    2022.02.28_10989번 문제풀이

---
+++ 10989번/10989.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int n,enter;
+	int cnt[10001] = { 0 };
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> enter;
+		cnt[enter]++;
+	}
+
+	for (int i = 1; i <= 10000; i++)
+	{
+		while (cnt[i])
+		{
+			cout << i << '\n';
+			cnt[i]--;
+		}
+	}
+}
