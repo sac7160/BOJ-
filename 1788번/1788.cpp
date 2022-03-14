a0953e65fb78a955d7e65cdba11fd5322ad4e7ad
ChangHyeon <sac7160@naver.com>
2022-03-14 17:38:10 +0900

    2022.03.14_1788번 문제풀이

---
+++ 1788번/1788.cpp
@@ -0,0 +1,41 @@
+﻿//1788번
+
+#include <iostream>
+
+using namespace std;
+
+int a[1000000] = { 0 };
+
+int fib(int n)
+{
+	if (n == 0) return 0;
+	else if (n == 1) return 1;
+	
+	if (a[n] == 0 ) a[n] = fib(n - 2) % 1000000000 + fib(n - 1) % 1000000000;
+
+	return a[n]%1000000000;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	if (n == 0) cout << 0 << '\n';
+	else if (n > 0) cout << 1 << '\n';
+	else if (n < 0)
+	{
+		if (-n % 2 == 0) cout << -1 << '\n';
+		else cout << 1 << '\n';
+	}
+
+	if (n >= 0)
+	{
+		cout << fib(n);
+	}
+	else if (n < 0)
+	{
+		cout << fib(-n);
+	}
+}
+
