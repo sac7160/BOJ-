6a43bd307a36253e2b38df8dcabf07d629391b18
ChangHyeon <sac7160@naver.com>
2022-07-16 11:43:54 +0900

    2022.07.16_11047번 문제풀이

---
+++ Greedy/11047번/11047.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+
+	int* coins = new int[n];
+
+	for (int i = 0; i < n; i++) cin >> coins[i];
+
+	int sum = 0;
+
+	for (int i = n-1; i >= 0; i--)
+	{
+		if (coins[i] <= k)
+		{
+			sum += k / coins[i];
+			k = k % coins[i];
+		}
+	}
+
+	cout << sum;
+
+	delete[] coins;
+}
